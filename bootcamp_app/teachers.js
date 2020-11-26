8c5ab41039e6826c23cd8867c73ef0f70462073e
Shaun Yap <me@shaunyap.ca>
2020-11-26 03:00:07 +0000

    Parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,19 @@ const pool = new Pool({
 });
 
 const input = process.argv.slice(2);
+const values = [input[0]];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${input[0]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
